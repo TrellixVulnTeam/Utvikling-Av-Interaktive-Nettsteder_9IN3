b09f0d4becd01d34ac74669999f9838d0ee06401
joakimvu <joakim.vu@hiof.no>
2022-01-15 10:31:38 +0100

    Update index.js

--- Oppgave 3/src/index.js
+++ Oppgave 3/src/index.js
@@ -40,23 +40,79 @@ const updateText = (event) => {
 input.addEventListener("keyup", updateText);
 
 // Oppgave 4
-
 const myList = ["item one", "item two", "item three"];
 
 const ul = document.getElementById("ul");
 const writeListBtn = document.getElementById("write-list");
 
 writeList = () => {
-    //myList.forEach((listElement) => (ul.innerHTML += `<li>${listElement}</li>`));
+    myList.forEach((listElement) => (ul.innerHTML += `<li>${listElement}</li>`));
 
-    const liElements = myList.map((listElement) => `<li>${listElement}</li>`);
-    ul.innerHTML = liElements.join(``);
 }
-
 
 writeListBtn.addEventListener("click",writeList);
 
+
 // Oppgave 5
+const select = document.getElementById("select");
+const text = document.getElementById("text");
+const htmlPlaceholder = document.getElementById("placeholder");
+const createBtn = document.getElementById("create");
+
+const createElement = () => {
+    const htmlEl = select.value;
+    const message = text.value;
+    htmlPlaceholder.innerHTML += `<${htmlEl}>${message}</${htmlEl}>`;
+}
+
+createBtn.addEventListener("click", createElement);
+
 // Oppgave 6
+const list = document.getElementById("list");
+const removeListBtn = document.getElementById("remove-li");
+
+const removeList = () => {
+    list.innerHTML = null;
+}
+
+removeListBtn.addEventListener("click", removeList);
+
+
 // Oppgave 7
+const inputName = document.getElementById("name");
+const orderBtn = document.getElementById("order");
+
+const handleKeyUp = () => {
+    const name = inputName.value;
+    
+    if(name.length > 4) {
+        orderBtn.disabled = true;
+        orderBtn.style = "border: 1px solid red";
+    } else {
+        orderBtn.disabled = false;
+        orderBtn.style = "border: 1px solid black";
+    }
+}
+
+inputName.addEventListener("keyup", handleKeyUp);
+
+
 // Oppgave 8
+const ulParent = document.querySelector(".children");
+const ulParentChildren = ulParent.querySelectorAll("li");
+const colorBtn = document.getElementById("color");
+
+const addBorder = () => {
+    Array.from(ulParentChildren).forEach((li, index) => {
+        console.log(li);
+        li.style = "border: 1px solid blue; margin-bottom: 10px; padding: 5px";
+        
+        // if((index + 1) % 2 === 0) {
+        //     li.style = "border: 1px solid pink; margin-bottom: 10px; padding: 5px;";
+        // } else {
+        //     li.style = "border: 1px solid green; margin-bottom: 10px; padding: 5px;";
+        // }
+    })
+}
+
+colorBtn.addEventListener("click", addBorder);
