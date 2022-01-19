724cf7577210c9a455a059152a689cd5feb7ec28
joakimvu <joakim.vu@hiof.no>
2022-01-19 19:10:07 +0100

    Update ingen_hjelp.js

--- Oppgave 3 JS/Oppgave 2/src/ingen_hjelp.js
+++ Oppgave 3 JS/Oppgave 2/src/ingen_hjelp.js
@@ -14,9 +14,10 @@ const users = [
 
 // TODO: Hent HTML #id med getElementById
  const searchInput = document.getElementById("name");
- const filtetrInput = document.getElementById("age");
+ const filterInput = document.getElementById("age");
  const filterButton = document.getElementById("filter");
  const userUl = document.getElementById("users");
+
 
 // TODO: Lag en funksjon som kan brukes til å skrive ut HTMLen for å se brukerene. Du kan bruke users importert over for å hente en liste med 10 brukere
 const createTableUI = (users) => {
@@ -26,10 +27,51 @@ const createTableUI = (users) => {
     userUl.innerHTML += `<li><span>${user.id}</span><span>${user.name}</span><span>${user.age}</span></li>`;
   }
 };
+
+
 // TODO: Lag en funksjon som håndterer søket og oppdaterer grensesnittet med resultatet fra søket
+const handleSearch = () => {
+  // Henter ut verdien fra søker inputet feltet
+  const searchName = searchInput.value;
+  // Sjekker om searchName eksisterer
+  if (searchName){
+    // Bruker .find for å finne den brukeren som har det navnet vi søker etter
+    // Gjør om alt til lowerCase for å gjøre søket "bedre"
+    const searchResult = users.find(
+      (user) => user.name.toLowerCase() === searchName.toLowerCase()
+    );
+    if(searchResult){
+      createTableUI([searchResult])
+    }else{
+      userUl.innerHTML = `Fant ingen med navnet ${searchName}`;
+    }
+  }else{
+    createTableUI(users);
+  }
+};
+
 // TODO: Lag en funksjon som håndterer filteret og oppdaterer grensesnittet med resultatet fra filteret
+const handleFilter = () => {
+  // Henter ut verdien fra filter input feltet
+  const filterValue = filterInput.value;
+  // Sjekker om filterValue eksisterer og er et tall
+  if(filterValue && Number(filterValue)){
+    // Sjekker om resultatet eksisterer
+    const filterResult = users.filter((user) => user.age > filterValue);
+    if(filterResult && filterResult.length > 0){
+      createTableUI(filterResult);
+    }else{
+      userUl.innerHTML = `Fant ingen over ${filterValue}`;
+    }
+  }else {
+    // Skrive ut default liste om filterverdien er tom
+    createTableUI(users);
+  }
+}
 // TODO: Lytt til tastatur klikk på søkefeltet, den skal trigge søkefunksjonen (handleSearch)
+searchInput.addEventListener("keyup", handleSearch);
 // TODO: Lytt til klikk på filter-knappen, den skal trigge filterfunksjonen (handleFilter)
+filterInput.addEventListener("click", handleFilter)
 
 const main = () => {
   createTableUI(users);
