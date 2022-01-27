97da10f71b0dc489e5aa6f696ae197b8018d4fb2
joakimvu <joakim.vu@hiof.no>
2022-01-27 14:27:17 +0100

    Ferdig opg 4

--- Oppgave 3 JS/Oppgave 4/src/mest_hjelp.js
+++ Oppgave 3 JS/Oppgave 4/src/mest_hjelp.js
@@ -1,3 +1,5 @@
+// import './styles.css'
+
 // TODO: Bruk getElementById til å hente HTML med #id
 // TODO: Bruk querySelector til å hente knappen
 const numbersUl = document.getElementById("numbers");
@@ -8,35 +10,35 @@ const numbers = [1, 5, 6, 3, 7, 2, 4];
 
 const getGuess = () => {
   // TODO: Bruk querySelectorAll på guessUl for å hente ut alle input feltene
-  const answers = document.querySelectorAll("guessUl");
+  const answers = document.querySelectorAll("input");
   // TODO: Bruk .map for å hente ut verdiene i input feltene
-  return Array.from(answers).map();
+  return Array.from(answers).map((answer) => answer.value);
 };
 
 const checkNumberSeq = () => {
   const guess = getGuess();
   let isCorrect = numbers.sort().join("") === guess.join("");
   if (isCorrect) {
-    alert("Du sorterte riktig");
+      alert("Du sorterte riktig");
+  }
+  if (!isCorrect) {
+      alert('Du sorterte feil')
   }
 };
 
 const addInputUI = () => {
   // TODO: Bruk for-of (eller vanlig for-løkke) og guessUl til å lage li-elementer med input elementer for å kunne skrive inn hva som skal sorteres
   for (let element of numbers) {
-    const li = `<li> ${element}</li>`;
+    const li = `<li><input></input></li>`;
     guessUl.innerHTML += li;
   }
-  console.log(guessUl);
-
 };
-
 
 const addNumbersUI = () => {
   // TODO: Bruk for-of (eller vanlig for-løkke) og numbersUl til å lage li-elementer med tallene som skal sorteres
   for (let value of numbers) {
     const li = value;
-    numbersUl.innerHTML += `${li}, `;
+    numbersUl.innerHTML += `<li>${li}</li>`
   }
 };
 
