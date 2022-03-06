f6526311e13ae371e3e72fe451f7cbf780b55793
joakimvu <joakim.vu@hiof.no>
2022-03-06 18:49:04 +0100

    kommentarer
    la til litt flere kommentarer

--- Oppgave 7/src/components/Main.js
+++ Oppgave 7/src/components/Main.js
@@ -9,38 +9,48 @@ function Main() {
   // http://www.omdbapi.com/?s=Star Wars&apikey=5cc65390
   const [movies, setMovies] = useState()
   const [search, setSearch] = useState()
+  const [notext, setNotext] = useState()
   // Bruker useEffect for å få vise 5 filmer med søkeorder Star Wars
   useEffect(() => {
     fetch('http://www.omdbapi.com/?s=Star%20Wars&apikey=5cc65390')
       .then((response) => response.json())
       .then((data) => setMovies(data.Search.splice(0, 5)))
-      .catch((err) => console.log(err))
+      .catch((error) => console.log(error))
   }, [])
 
   // Bruker async for å hente data fra OmdbApi asynkront
   // `http://www.omdbapi.com/?s=${search}&apikey=5cc65390`
   const handleSearch = async (event) => {
     event.preventDefault()
-    const response = await fetch(
-      `http://www.omdbapi.com/?s=${search}&apikey=5cc65390`
-    )
-    // Bruker try/catch for å håndtere eventuelle feil som kan oppstå når vi konverterer
-    // til . json()
-    try {
-      const data = await response.json()
-      setMovies(data.Search)
-    } catch (error) {
-      // logger feilmeldingen
-      console.log(error)
+    if (search) {
+      const response = await fetch(
+        `http://www.omdbapi.com/?s=${search}&apikey=5cc65390`
+      )
+      // Bruker try/catch for å håndtere eventuelle feil som kan oppstå når vi konverterer
+      // til . json()
+      try {
+        const data = await response.json()
+        setMovies(data.Search)
+        setNotext('')
+      } catch (error) {
+        // logger feilmeldingen
+        console.log(error)
+      }
+    } else {
+      setNotext('Søk på en film tittel')
     }
   }
 
+  // Funksjon som håndterer det som blir skrevet inn i søkefeltet
   const handleInput = (event) => {
     setSearch(event.currentTarget.value)
   }
 
   return (
     <main>
+      <p style={{ color: 'red' }} className="notext">
+        {notext}
+      </p>
       <Search
         search={search}
         setSearch={setSearch}
