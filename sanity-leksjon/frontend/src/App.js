6e3c7501d2678311b252cf13233fed7d7edaaa5c
joakimvu <joakim.vu@hiof.no>
2022-03-10 21:09:07 +0100

    ferdig med leksjon

--- sanity-leksjon/frontend/src/App.js
+++ sanity-leksjon/frontend/src/App.js
@@ -1,12 +1,25 @@
-import MyComponent from './components/MyComponent'
+import { getEvent, getEvents } from './utils/eventService'
 
 export default function App() {
-  // Write JavaScript, use Hooks, add state and more
+  const getEventsData = async () => {
+    const events = await getEvents()
+    console.log(JSON.stringify(events))
+  }
+
+  const getEventData = async () => {
+    const slug = 'event-1'
+    const event = await getEvent(slug)
+    console.log(JSON.stringify(event))
+  }
 
   return (
-    <main className="mx-auto mt-6 max-w-sm border-2 p-6">
-      <p className="text-3xl font-bold underline">Legg til JSX</p>
-      <MyComponent />
+    <main>
+      <button type="button" onClick={getEventsData}>
+        Hent Data
+      </button>
+      <button type="button" onClick={getEventData}>
+        Hent data om et event
+      </button>
     </main>
   )
 }
